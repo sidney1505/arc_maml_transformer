0c6ecb3f5feef34812d158da76cbad019b367791
sidney <sb.sidney.bender@gmail.com>
2020-05-19 12:50:08 +0200

    added description how to execute the project

---
+++ setup_cpu.sh
@@ -0,0 +1,4 @@
+# download data
+virtualenv -p python3.7 cpu_env
+source cpu_env/bin/activate
+pip install -r requirements_cpu.txt
---
+++ setup_gpu.sh
@@ -0,0 +1,4 @@
+# download data
+virtualenv -p python3.7 gpu_env
+source gpu_env/bin/activate
+pip install -r requirements_gpu.txt
